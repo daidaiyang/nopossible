48bcb2f30138b63c771dc58290c87532672ff2fe
daidaiyang <2991233394@qq.com>
2019-02-21 19:28:33 +0800

    已有界面搭建完毕申购只搭建了界面没有搭建Activity

---
+++ app/src/main/java/com/nopossible/activities/findpass/FindpassContract.java
@@ -0,0 +1,21 @@
+package com.nopossible.activities.findpass;
+
+import android.content.Context;
+
+import com.nopossible.mvp.BasePresenter;
+import com.nopossible.mvp.BaseView;
+
+/**
+ * MVPPlugin
+ *  邮箱 784787081@qq.com
+ */
+
+public class FindpassContract {
+    interface View extends BaseView {
+        
+    }
+
+    interface  Presenter extends BasePresenter<View> {
+        
+    }
+}
---
+++ app/src/main/java/com/nopossible/activities/findpass/FindpassPresenter.java
@@ -0,0 +1,14 @@
+package com.nopossible.activities.findpass;
+
+import android.content.Context;
+
+import com.nopossible.mvp.BasePresenterImpl;
+
+/**
+ * MVPPlugin
+ *  邮箱 784787081@qq.com
+ */
+
+public class FindpassPresenter extends BasePresenterImpl<FindpassContract.View> implements FindpassContract.Presenter{
+    
+}
